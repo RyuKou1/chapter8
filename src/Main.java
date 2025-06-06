5b6908c9d43a9a913c71371a860d7753d198adde
241105 <241105@std.hi-joho.ac.jp>
2025-06-06 12:17:17 +0900

    chapter8

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
-// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
-public class Main {
+ public class Main{
     public static void main(String[] args) {
         Hero h = new Hero();
         h.name = "ミナト";

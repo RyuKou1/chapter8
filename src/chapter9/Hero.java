fed97f6afec3f854f04f99b0bbccaaff7f421598
241105 <241105@std.hi-joho.ac.jp>
2025-06-11 09:32:08 +0900

    chapter8

--- src/chapter9/Hero.java
+++ src/chapter9/Hero.java
@@ -9,12 +9,31 @@ public class Hero {
         System.out.println("敵に５ポイントのダメージをあたえた！");
     }
 
-    public Hero(String name){
+    // 引数なし
+    public Hero() {//同じ名前
+        this.hp = 100;
+        this.name = "ダミー";
+        //this(100, "ダミー");
+        System.out.println("引数なし");
+    }
+    // 引数あり String
+    public Hero(String name) {//同じ名前
         this.hp = 100;
         this.name = name;
+        //this(100, name);
+        System.out.println("引数あり String");
     }
-
-    public Hero(){
-        this("ダミー");
+    // 引数あり int
+    public Hero(int hp) {//同じ名前
+        this.hp = hp;
+        this.name = "ダミー";
+        //this(hp, "ダミー");
+        System.out.println("引数あり int");
+    }
+    // 引数2つ
+    public Hero(int hp, String name) {//同じ名前
+        this.hp = hp;
+        this.name = name;
+        System.out.println("引数2つ");
     }
 }
